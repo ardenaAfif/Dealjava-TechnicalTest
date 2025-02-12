2861af099a65572af4a2700103e708220b2ef6a4
aafif <aafifpratama@gmail.com>
2025-02-12 11:43:17 +0700

    Added box ingredient opened

--- app/src/main/java/com/afif/test/dealjavatechnicaltest/adapter/IngredientBoxAdapter.kt
+++ app/src/main/java/com/afif/test/dealjavatechnicaltest/adapter/IngredientBoxAdapter.kt
@@ -3,12 +3,18 @@ package com.afif.test.dealjavatechnicaltest.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.afif.test.dealjavatechnicaltest.R
 import com.afif.test.dealjavatechnicaltest.data.IngredientBox
 import com.afif.test.dealjavatechnicaltest.databinding.ItemIngredientBinding
 import com.dotlottie.dlplayer.Mode
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.lottiefiles.dotlottie.core.model.Config
 import com.lottiefiles.dotlottie.core.util.DotLottieSource
+import com.lottiefiles.dotlottie.core.widget.DotLottieAnimation
 
 class IngredientBoxAdapter(private val context: Context, private val ingredientBox: List<IngredientBox>) :
     RecyclerView.Adapter<IngredientBoxAdapter.IngredientViewHolder>() {
@@ -39,10 +45,40 @@ class IngredientBoxAdapter(private val context: Context, private val ingredientB
     }
 
     override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
-        holder.bind(ingredientBox[position])
+        val ingredientBox = ingredientBox[position]
+        holder.bind(ingredientBox)
+        holder.itemView.setOnClickListener {
+            showBottomSheetDialog(ingredientBox)
+        }
     }
 
     override fun getItemCount(): Int {
         return ingredientBox.size
+    }
+
+    private fun showBottomSheetDialog(ingredientBox: IngredientBox) {
+        val bottomSheetDialog = BottomSheetDialog(context)
+        val bottomSheetView = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_box, null)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        val dotLottieAnimation = bottomSheetView.findViewById<DotLottieAnimation>(R.id.lottieGiftSheet)
+        val ingredientName = bottomSheetView.findViewById<TextView>(R.id.nameBox)
+        val btnOpenBox = bottomSheetView.findViewById<Button>(R.id.btnOpenBox)
+
+        val config = Config.Builder()
+            .autoplay(true)
+            .speed(1f)
+            .loop(true)
+            .source(DotLottieSource.Asset(ingredientBox.lottieFile))
+            .playMode(Mode.BOUNCE)
+            .build()
+        dotLottieAnimation.load(config)
+        ingredientName.text = ingredientBox.name
+        btnOpenBox.setOnClickListener {
+            bottomSheetDialog.dismiss()
+            Toast.makeText(context, "${ingredientBox.name} opened", Toast.LENGTH_SHORT).show()
+        }
+
+        bottomSheetDialog.show()
     }
 }
